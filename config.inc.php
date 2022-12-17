c5cf285c0d56c55119aa74143ad0de3c4fbabeb0
ze yuan lin <62914599+287775856@users.noreply.github.com>
2022-12-17 21:11:33 +0800

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -43,6 +43,6 @@ $db->addServer(array (
   'charset' => 'utf8mb4',
   'port' => '3306',
   'database' => 'blog',
-  'engine' => 'MyISAM',
+  'engine' => 'InnoDB',
 ), Typecho_Db::READ | Typecho_Db::WRITE);
 Typecho_Db::set($db);
